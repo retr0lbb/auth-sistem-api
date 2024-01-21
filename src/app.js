afc5e4417b3e85f52c9e33fbb431877573895613
Henrique Barbosa Sampaio <retr0lbb@gmail.com>
2024-01-21 14:32:43 -0300

    refactor: added hidden logic remake env

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import route from "./modules/auth/routes/authRouter.js"
 import DatabaseConnect from './database/connection.js';
 import cors from "cors"
 import express from "express";
+import {configDotenv} from "dotenv"
+configDotenv()
 const app = express();
 const connection = DatabaseConnect()
 
@@ -25,4 +27,5 @@ app.get("/", (req, res)=>{
 
 app.listen(port, ()=>{
     console.log(`Server running on port: ${port}`);
+    console.log(process.env.MONGOURI);
 });
